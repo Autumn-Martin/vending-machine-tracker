3622936c9508715443bddf2c9ad0fb0440aedc36
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-09-07 10:59:33 -0600

    fix test

--- spec/features/user_can_see_vending_machine_show_page_spec.rb
+++ spec/features/user_can_see_vending_machine_show_page_spec.rb
@@ -7,13 +7,17 @@ feature 'User can see vending machine show page' do
   context 'user visits machines/id' do
     scenario 'user sees a list of names and prices for that vending machine' do
       sam = Owner.create(name: "Sam's Snacks")
-      bot = sam.machines.create(location: "Turing Basement")
-      item_1 = sam.machines.items.create(name: "Reesees", price: "$1")
-      item_2= sam.machines.items.create(name: "Reesees", price: "$1")
+      bot = sam.machines.create(location: "Turing Basement", id: 1)
+      item_1 = Item.create(name: "Reesees", price: 1.25, machine_id: 1)
+      item_2= Item.create(name: "Oreos", price: 1.75, machine_id: 1)
 
-      visit machines_path(bot)
 
-      expect(page).to have_content()
+      visit machine_path(bot)
+
+      expect(page).to have_content("#{item_1.price}")
+      expect(page).to have_content(item_1.name)
+      expect(page).to have_content("#{item_2.price}")
+      expect(page).to have_content(item_2.name)
     end
   end
 end
